26cad96d88394ced5ac080eab4e4d6138324d68c
oooo91 <74234719+oooo91@users.noreply.github.com>
2022-12-31 16:37:43 +0900

    Revert "Feature/join"

--- src/main/java/com/portfolio/postproject/user/entity/DiaryUserCode.java
+++
@@ -1,9 +0,0 @@
-package com.portfolio.postproject.user.entity;
-
-public interface DiaryUserCode {
-
-    //user_status
-    String STATUS_READY = "READY";
-    String STATUS_ACTIVE = "ACTIVE";
-    String STATUS_STOP = "STOP";
-}
